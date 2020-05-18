f3a13359bb696ec4745ef943d85bfd8eae8de229
yangl-china <854151366@qq.com>
2020-05-18 12:18:32 +0800

    v2.3

--- 国新办/JS/xigovernance-m.js
+++ 国新办/JS/xigovernance-m.js
@@ -29,7 +29,8 @@
     "Aid": 'w27',
     "Two Sessions 2018": 'w28',
     "Press Minutes": 'w29',
-    "2019 NPC & CPPCC": 'w30'
+    "2019 NPC & CPPCC": 'w30',
+    "2020 NPC & CPPCC": 'w31'
     };
     $('.mmmm').each(function () {
             var curTitle = $.trim($(this).text());
