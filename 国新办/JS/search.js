48d45ff98741a488a0516ae01fb0f0bf0d22f27b
yangl-china <854151366@qq.com>
2020-06-30 15:07:23 +0800

    v2.4

--- 国新办/JS/search.js
+++ 国新办/JS/search.js
@@ -145,7 +145,8 @@
             "Aid": 'w27',
             "Two Sessions 2018": 'w28',
             "Press Minutes": 'w29',
-            "2019 NPC & CPPCC": 'w30'
+            "2019 NPC & CPPCC": 'w30',
+            "2020 NPC & CPPCC": 'w31'
         };
         $('.mmmm').each(function () {
             var curTitle = $.trim($(this).text());
