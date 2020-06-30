cfb1cc7f1d1d556934b7ff7c2a5755a2c507d1a7
yangl-china <854151366@qq.com>
2020-06-30 15:01:42 +0800

    v1.0

---
+++ 关键词/重要/搜索页/pmtkcde_search.js
@@ -0,0 +1,118 @@
+(function ($) {
+    var startpage = getQueryString('page') ? parseInt(getQueryString('page')) : 1;
+    var searchText = getQueryString('searchText') ? getQueryString('searchText') : '';
+    var sub = getQueryString('sub');
+    var pageSize = 40;
+    var totalPage = 0;
+    var getUrl = '';
+    if (sub) {
+        getUrl = 'http://search.china.org.cn/hlftiweb/en/jsonfn.jsp?strUrl=pmtkcde.org.cn&channel=1&page=' + startpage + '&nItem=' + pageSize + '&sub=' + sub;
+    } else {
+        getUrl = 'http://search.china.org.cn/hlftiweb/en/jsonfn.jsp?strUrl=pmtkcde.org.cn&channel=1&page=' + startpage + '&nItem=' + pageSize + '&searchText=' + searchText;
+    }
+
+    $.getScript(getUrl);
+    window.searchRes = function (data) {
+        var len = data.size;
+        var pageHref = [];
+        var temp = [];
+        totalPage = Math.ceil(len / pageSize);
+        $('#yText').html(searchText)
+        $('#yInfo').html('Results ' + (startpage * pageSize - pageSize + 1) + ' - ' + startpage * pageSize + ' of ' + len);
+        for (var i = 0; i < data.list.length; i++) {
+            var re=new RegExp(" amp ","g");
+            var re1=new RegExp("BampR","g");
+            var newstr="";
+            var newsub="";
+            if(data.list[i].abstract.match(re)){
+               newstr=data.list[i].abstract.replace(re," & "); 
+            }else if(data.list[i].abstract.match(re1)){
+                newstr=data.list[i].abstract.replace(re1,"B&R");
+            }else{
+                newstr=data.list[i].abstract;
+            }
+            if(data.list[i].sub.match(re)){
+               newsub=data.list[i].sub.replace(re," & "); 
+            }else{
+                newsub=data.list[i].sub;
+            }
+            temp.push('<li><p>' + data.list[i].title + '</p><span>'+data.list[i].sub+'</span></li>');
+        }
+        $('#yList').html(temp.join(''));
+        pageObj();
+    };
+
+    function pageObj() {
+        var i, key = '';
+        var pageHref = [];
+        if (sub) {
+            key = 'sub';
+        } else {
+            key = 'searchText';
+        }
+        var txt = getQueryString(key) ? getQueryString(key) : '';
+
+        var pagination_buf = [];
+        // prev
+        pagination_buf.push(startpage > 1 ? '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + (startpage - 1) + '&' + key + '=' + txt + '">Prev</a>' : '<span>Prev</span>');
+        // pagination
+        if (totalPage <= 10) {
+            for (i = 1; i <= totalPage; i++) {
+                pagination_buf.push(i == startpage ? '<span>' + i + '</span>' : '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+            }
+        } else {
+            // total page > 10
+            if (startpage <= 4) {
+                // 1 2 3 4 5 ... 8 9 10 11
+                for (i = 1; i <= 5; i++) {
+                    pagination_buf.push(i == startpage ? '<span>' + i + '</span>' : '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+                pagination_buf.push('<span>...</span>');
+                for (i = totalPage - 2; i <= totalPage; i++) {
+                    pagination_buf.push('<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+            } else if (startpage >= totalPage - 3) {
+                // 1 2 3 ... 7 8 9 10 11
+                for (i = 1; i <= 3; i++) {
+                    pagination_buf.push('<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+                pagination_buf.push('<span>...</span>');
+                for (i = totalPage - 4; i <= totalPage; i++) {
+                    pagination_buf.push(i == startpage ? '<span>' + i + '</span>' : '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+            } else {
+                // 1 2 ... 5 6 7 ... 9 10 11
+                for (i = 1; i <= 2; i++) {
+                    pagination_buf.push('<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+                pagination_buf.push('<span>...</span>');
+                for (i = startpage - 1; i <= startpage + 1; i++) {
+                    pagination_buf.push(i == startpage ? '<span>' + i + '</span>' : '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+                pagination_buf.push('<span>...</span>');
+                for (i = totalPage - 1; i <= totalPage; i++) {
+                    pagination_buf.push('<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + i + '&' + key + '=' + txt + '">' + i + '</a>');
+                }
+            }
+        }
+        // next
+        pagination_buf.push(startpage < totalPage ? '<a href="?strUrl=pmtkcde.org.cn&channel=1&page=' + (startpage + 1) + '&' + key + '=' + txt + '">Next</a>' : '<span>Next</span>');
+        $('#autopage').html(pagination_buf.join(''));
+    }
+
+    function sortby(int) {
+        if (int == 1) {
+            window.location.href = location.href + '&strSortBy=1';
+        } else {
+            window.location.href = location.href + '&strSortBy=2';
+        }
+    }
+
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return window.decodeURIComponent(r[2]); return null;
+    }
+})(jQuery);
+
+
